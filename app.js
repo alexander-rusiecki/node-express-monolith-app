4746e9a9707fde35fe5d723fc6bbc871d98edb42
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-05-11 16:36:18 +0200

    set up server & use middleware & set up basic routes

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const booksRoute = require('./routes/booksRoute');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(booksRoute);
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404 - Not found' });
+});
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
